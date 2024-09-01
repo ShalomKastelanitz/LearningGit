5b6fa44fa76911e95e8f285e96c90d5c3d4359c4
This User <This User@DESKTOP-L9S4R74>
2024-09-01 14:39:36 +0300

    Add project files.

---
+++ LearningGit/Controllers/recipesController.cs
@@ -0,0 +1,37 @@
+﻿using LearningGit.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearningGit.Controllers
+{
+    public class recipesController : Controller
+    {
+        private List<Recipes> recipes = new List<Recipes>
+        {
+            new Recipes
+            {
+                Id = 1,
+                Name = "Recipe 1",
+                Description = "Short description for recipe 1",
+                PreparationTime = 30,
+                ProductList = new List<string> { "Flour", "Sugar", "Eggs", "Butter" },
+                StepsToPerform = new List<string> { "Mix ingredients", "Bake at 180°C for 30 minutes" }
+            },
+            new Recipes
+            {
+                Id = 2,
+                Name = "Recipe 2",
+                Description = "Short description for recipe 2",
+                PreparationTime = 45,
+                ProductList = new List<string> { "Pasta", "Tomato Sauce", "Cheese" },
+                StepsToPerform = new List<string> { "Boil pasta", "Prepare sauce", "Mix and serve" }
+            },
+            // Add more recipes here
+        };
+
+        public ActionResult Index()
+        {
+            return View(recipes);
+        }
+
+    }
+}
---
+++ LearningGit/Models/Recipes.cs
@@ -0,0 +1,12 @@
+﻿namespace LearningGit.Models
+{
+    public class Recipes
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double PreparationTime { get; set; }
+        public List<string> ProductList { get; set; }
+        public List<string> StepsToPerform { get; set; }
+    }
+}
