006d8fdf275ce2aaf666da0a2b5cdda3bcbd3a37
ShalomKastelanitz <5713133@gmail.com>
2024-09-03 17:32:00 +0300

    Add files via upload
    עדכון

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using data_structures;
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+       LinkedList myLinkedList = new LinkedList();
+        myLinkedList.Add(1);
+        myLinkedList.Add(2);
+        myLinkedList.Add(3);
+            
+        myLinkedList.Add(4);
+       Console.WriteLine( myLinkedList.Display());
+
+
+
+    }
+   
+    }
+   
+
+
